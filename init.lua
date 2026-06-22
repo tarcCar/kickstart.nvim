45d1cddb4915b1916b86f06d7f518976f89e69ba
Tarcísio Mulatti <tarcisio.mulatti@trinca.com>
2026-06-22 08:50:49 -0300

    feat: change the path display to smart

--- init.lua
+++ init.lua
@@ -394,6 +394,9 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = {
+          path_display = { "smart" },
+        },
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
         },
